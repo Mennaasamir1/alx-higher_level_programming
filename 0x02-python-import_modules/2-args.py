8213aa98abaa178abf35923e9ab6805f2b3d7063
Mennaasamir1 <monyy_20174@outlook.com>
2024-05-03 21:34:52 +0300

    new task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     length = len(sys.argv) - 1
-    if length < 1:
+    if length == 0:
         print("{} arguments.".format(length))
     elif length == 1:
         print("{} argument:".format(length))
