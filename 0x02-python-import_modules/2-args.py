76ea3b14ff89ff72776b8cf4ac4ce43df1fec453
Mennaasamir1 <monyy_20174@outlook.com>
2024-05-04 00:16:16 +0300

    task 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     total = len(argv) - 1
-    if total == 0:
+    if total < 1:
         print("0 arguments.")
     elif total == 1:
         print("1 argument:")
