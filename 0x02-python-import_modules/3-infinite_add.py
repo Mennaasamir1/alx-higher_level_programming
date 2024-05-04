bee1a028ee8bfde4f6332a06bc74081d1ffa583e
Mennaasamir1 <monyy_20174@outlook.com>
2024-05-05 01:09:38 +0300

    t

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,8 +2,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    length = len(sys.argv)
     result = 0
-    for i in range(1, length):
+    for i in range(1, len(sys.argv)):
         result += int(sys.argv[i])
     print("{}".format(result))
