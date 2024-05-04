8ab64feeaa9af463f95e4a8aad81bc7cc8b28f31
Mennaasamir1 <monyy_20174@outlook.com>
2024-05-04 23:16:33 +0300

    t

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     count = len(argv) - 1
-    if count < 1:
+    if count == 0:
         print("{} argument.".format(count))
     elif count == 1:
         print("{} arguments:".format(count))
