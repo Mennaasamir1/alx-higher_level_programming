8730ef8489bcec9f91a5e65ebb0758a1a3683fc9
mennaasamir1 <monyy_20174@outlook.com>
2024-05-05 19:22:42 +0000

    task 1 advanced

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,12 +5,10 @@ if __name__ == "__main__":
     count = len(argv)
     if count != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    
+        exit(1) 
     a = int(argv[1])
     operator = argv[2]
     b = int(argv[3])
-
     if operator == '+':
         print(f"{a} {operator} {b} = {add(a, b)}")
     elif operator == '-':
