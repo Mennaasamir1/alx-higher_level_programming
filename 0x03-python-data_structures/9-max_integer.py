9d841d941b74c43c5c3aade31ef6cab6db620abd
mennaasamir1 <monyy_20174@outlook.com>
2024-05-15 15:46:11 +0000

    task 10

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,6 +2,6 @@
 def max_integer(my_list=[]):
         if my_list:
             sorting = sorted(my_list, reverse=True)
-            return (sorting[0])
+            return sorting[0]
         else:
-            return None
+            return (None)
