f1a015c7d3caeb9897a15f593cadedfcd5f88329
mennaasamir1 <monyy_20174@outlook.com>
2024-05-17 21:14:13 +0000

    adv task

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,19 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+
+void print_python_list_info(PyObject *p)
+{
+    long int size_of_list = PyList_Type(p);
+    int i;
+    PyListObject *j = (PyListObject *) p;
+
+    printf("[*] Size of the Python List = %li\n", size_of_list);
+    printf("[*] Allocated = %li\n", j->allocated);
+
+    for(i = 0; i < size; i++)
+    {
+        printf("Element %i: %s\n", i, PY_TYPE(j->ob_item[i]->tp_name);)
+    }
+
+}
