bbd98a5d7403c910c36d7b06578e61da8b9ba639
mennaasamir1 <monyy_20174@outlook.com>
2024-05-18 19:05:58 +0000

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if matrix:
-        new_list = [[element ** 2 for element in row] for row in matrix]
+        new_list = [[element ** 2 for element in row]
+                    for row in matrix]
         return new_list
