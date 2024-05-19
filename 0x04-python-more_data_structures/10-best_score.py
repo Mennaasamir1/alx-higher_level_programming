c9015255203f21026cf634d04f2051d8b3445091
mennaasamir1 <monyy_20174@outlook.com>
2024-05-19 19:00:47 +0000

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@ def best_score(a_dictionary):
        max_value = next(iter(a_dictionary))
        for key in a_dictionary:
            if a_dictionary[key] > a_dictionary[max_value]:
-              max_value = key 
+              max_value = key
        return max_value
     else:
-       return (None)
+       return None
