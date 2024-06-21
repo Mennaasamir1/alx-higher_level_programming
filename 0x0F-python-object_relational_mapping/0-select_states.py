01d26a0003ef6b9ac424bb18979e51f1de677ea1
email <monyy_20174@outlool.com>
2024-06-21 07:29:16 -0700

    t

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     db = MYSQLdb.connect(host="localhost", user=sys.argv[1],
-                        passwd=sys.argv[2], db=sys.argv[3], port=3386)
+                        passwd=sys.argv[2], db=sys.argv[3], port=3306)
     c = db.cursor()
     c.execute("SELECT * FROM states")
     rows = c.fetchall()
