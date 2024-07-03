d22585c5d49ee914fdd19c27b65bcfb3848321c2
mennaasamir1 <monyy_20174@outlook.com>
2024-07-03 22:45:53 +0000

    new task

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value is int:
-            return True
-    except TypeError:
+        print("{}".format(value))
+        return True
+    except (TypeError, ValueError):
         return False
