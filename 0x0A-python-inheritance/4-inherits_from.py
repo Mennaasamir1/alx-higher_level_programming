be11688da96958e7e10fdf38ee9a6d4bd3282f98
mennaasamir1 <monyy_20174@outlook.com>
2024-07-05 15:21:50 +0000

    new task

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""
-module only sub class of
-"""
+"""Defines a function inherits_from()"""
 
 
 def inherits_from(obj, a_class):
-    """returns True if the object is an instance of a class
-       that inherited (directly or indirectly) from the specified class;
-       otherwise False.
+    """Returns True if the object is an instance of a class that inherited,
+    (directly or indirectly) from the specified class ; otherwise False.
+
+    Args:
+        obj (a_class): object to check type.
+        a_class (type): type of type to check.
+
+    Returns:
+        boolean: True or False..
     """
-    if type(obj) != a_class:
-        return issubclass(type(obj), a_class)
-    else:
+    if type(obj) is a_class:
         return False
+    return issubclass(type(obj), a_class)
